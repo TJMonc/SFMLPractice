5ed21554b0b75850b9e83b282fffa923d9cc01ff
Terrance Moncure <terrancemoncure873@gmail.com>
2024-08-29 18:42:50 -0500

    Cmake config

---
+++ Build/CMakeFiles/SFMLPractice.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/terra/Downloads/Projects/repos/VSCodeRepos/SFMLPractice/src/Bullet.cpp" "CMakeFiles/SFMLPractice.dir/src/Bullet.cpp.obj" "gcc" "CMakeFiles/SFMLPractice.dir/src/Bullet.cpp.obj.d"
+  "C:/Users/terra/Downloads/Projects/repos/VSCodeRepos/SFMLPractice/src/Character.cpp" "CMakeFiles/SFMLPractice.dir/src/Character.cpp.obj" "gcc" "CMakeFiles/SFMLPractice.dir/src/Character.cpp.obj.d"
+  "C:/Users/terra/Downloads/Projects/repos/VSCodeRepos/SFMLPractice/src/CharacterContainer.cpp" "CMakeFiles/SFMLPractice.dir/src/CharacterContainer.cpp.obj" "gcc" "CMakeFiles/SFMLPractice.dir/src/CharacterContainer.cpp.obj.d"
+  "C:/Users/terra/Downloads/Projects/repos/VSCodeRepos/SFMLPractice/src/Game.cpp" "CMakeFiles/SFMLPractice.dir/src/Game.cpp.obj" "gcc" "CMakeFiles/SFMLPractice.dir/src/Game.cpp.obj.d"
+  "C:/Users/terra/Downloads/Projects/repos/VSCodeRepos/SFMLPractice/src/Main.cpp" "CMakeFiles/SFMLPractice.dir/src/Main.cpp.obj" "gcc" "CMakeFiles/SFMLPractice.dir/src/Main.cpp.obj.d"
+  "C:/Users/terra/Downloads/Projects/repos/VSCodeRepos/SFMLPractice/src/Misc.cpp" "CMakeFiles/SFMLPractice.dir/src/Misc.cpp.obj" "gcc" "CMakeFiles/SFMLPractice.dir/src/Misc.cpp.obj.d"
+  "C:/Users/terra/Downloads/Projects/repos/VSCodeRepos/SFMLPractice/src/Ship.cpp" "CMakeFiles/SFMLPractice.dir/src/Ship.cpp.obj" "gcc" "CMakeFiles/SFMLPractice.dir/src/Ship.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,7 @@
 #include "Game.h"
 #include <Windows.h>
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd) {
+int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
 	srand((int)std::time(NULL));
 
 	Game::Game* game = new Game::Game;
@@ -9,4 +9,5 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 	game->update();
 
 	delete game;
+	return 0;
 }
