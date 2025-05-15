63882505c0b7a2dada10ff1943f8335db594090e
Terrance Moncure <terrancemoncure873@gmail.com>
2025-05-14 21:36:17 -0500

    added dlls for the standard library.

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 cd "C:\Users\terra\Downloads\Projects\repos\SFMLPractice"
 cmake -G "MinGW Makefiles" -S . -B "./Build" -D CMAKE_CXX_COMPILER="C:\msys64\mingw32(13.1.0)\bin\g++.exe"
+
 cmake --build Build
 cd bin
