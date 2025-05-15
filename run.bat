818bc21a7b096ec31d41fb6e6d006232dc2f4169
Terrance Moncure <terrancemoncure873@gmail.com>
2025-05-14 20:12:34 -0500

    Redid some CMake Stuff

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 cd "C:\Users\terra\Downloads\Projects\repos\SFMLPractice"
-cmake -G "MinGW Makefiles" -S . -B "./Build"
+cmake -G "MinGW Makefiles" -S . -B "./Build" -D CMAKE_CXX_COMPILER="C:\msys64\mingw32(13.1.0)\bin\g++.exe"
 cmake --build Build
 cd bin
-start SFMLPractice.exe
